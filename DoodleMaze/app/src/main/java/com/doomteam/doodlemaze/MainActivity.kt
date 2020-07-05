597d3d5feeedc8fce77107123d77bea2fa4d85a3
Lee Rice <lee.rice7@gmail.com>
2020-07-04 19:02:09 -0600

    Integrated image processing code.

---
+++ DoodleMaze/app/src/main/java/com/doomteam/doodlemaze/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.doomteam.doodlemaze
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun openLoadMazeScreen(view: View) {
+        val loadMazeIntent = Intent (this, LoadMaze::class.java)
+        startActivity(loadMazeIntent)
+    }
+
+    fun openCreateMazeScreen(view: View) {
+        val createMazeIntent = Intent (this, BuildMazeActivity::class.java)
+        startActivity(createMazeIntent)
+    }
+}
