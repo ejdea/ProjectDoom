0c4906c4ecc920dceff63f3abf6443ba25093a28
Edmund Dea <deae@oregonstate.edu>
2020-07-21 01:01:26 -0700

    UnityComponent: Add audio/visual to start and end scenes. Shorten intro to Victory music.
    Signed-off-by: Edmund Dea <deae@oregonstate.edu>

--- UnityComponent/DoomMaze/Assets/Scripts/EndScript.cs
+++ UnityComponent/DoomMaze/Assets/Scripts/EndScript.cs
@@ -5,18 +5,44 @@ Date: 07/07/2020
 Version: 1.0
 */
 
+using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 
 // Script that handles game logic whenever the player reaches the end of the maze
 public class EndScript : MonoBehaviour
 {
     private bool isEnabled = false;
+    public VideoPlayer videoPlayer;
+    public AudioSource audioSource;
+    GameObject uiContainer;
+    private bool initialized = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        /* The preparation consists of reserving the resources needed for
+         * playback, and preloading some or all of the content to be played.
+         * After this is done, frames can be received immediately and all
+         * properties related to the source can be queried.
+         */
+        GameObject video = GameObject.Find("EndVideoPlayer");
+        if (video)
+		{
+            videoPlayer = video.GetComponent<VideoPlayer>();
+            videoPlayer.Prepare();
+            audioSource = videoPlayer.GetComponent<AudioSource>();
+        }        
+
+        // Get UIContainer
+        uiContainer = GameObject.Find("EndUIContainer");
+        if (uiContainer)
+		{
+            uiContainer.SetActive(false);
+        }
+
         // Disable screen dimming
         Screen.sleepTimeout = SleepTimeout.NeverSleep;
     }
@@ -24,7 +50,12 @@ public class EndScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!initialized && videoPlayer && videoPlayer.isPlaying)
+		{
+            audioSource.Play();
+            uiContainer.SetActive(true);
+            initialized = true;
+        }
     }
 
     // Set whether the end box is enabled
---
+++ UnityComponent/DoomMaze/Assets/Scripts/StartSceneScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class StartSceneScript : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public AudioSource audioSource;
+    GameObject uiContainer;
+    private bool initialized = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /* The preparation consists of reserving the resources needed for
+         * playback, and preloading some or all of the content to be played.
+         * After this is done, frames can be received immediately and all
+         * properties related to the source can be queried.
+         */
+        GameObject video = GameObject.Find("StartVideoPlayer");
+        if (video)
+        {
+            videoPlayer = video.GetComponent<VideoPlayer>();
+            videoPlayer.Prepare();
+            audioSource = videoPlayer.GetComponent<AudioSource>();
+        }
+
+        // Get UIContainer
+        uiContainer = GameObject.Find("StartUIContainer");
+        if (uiContainer)
+        {
+            uiContainer.SetActive(false);
+        }
+
+        // Disable screen dimming
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!initialized && videoPlayer.isPlaying)
+        {
+            audioSource.Play();
+            uiContainer.SetActive(true);
+            initialized = true;
+        }
+    }
+}
