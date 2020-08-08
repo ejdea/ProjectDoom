d22968878625ef8a81aa283bf834b13c0b6a6735
ltr7 <ltr7@users.noreply.github.com>
2020-08-07 18:56:59 -0600

    Added ability for mazes to be deleted

--- DoodleMaze/app/src/main/java/com/doomteam/doodlemaze/MainActivity.kt
+++ DoodleMaze/app/src/main/java/com/doomteam/doodlemaze/MainActivity.kt
@@ -3,7 +3,6 @@ package com.doomteam.doodlemaze
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.TextView
